d49824233670b1ed7f657ab8677c1e8f0e975cee
Ikeuchi Ryuto <ryu.i7133@gmail.com>
2021-04-13 17:17:29 +0900

    loading画面を追加

---
+++ components/Loading.js
@@ -0,0 +1,28 @@
+export const Loading = () => {
+  return (
+		<article className="w-2/5 border-4">
+			<div class="rounded-md p-4 max-w-sm w-full mx-auto">
+				<div class="animate-pulse space-x-4">
+					<div class="flex-1 space-y-4 py-1">
+						<div class="h-8 bg-gray-600 mx-auto rounded w-1/3"></div>
+						<div class="h-4 bg-gray-400 rounded w-3/4"></div>
+						<div class="h-4 bg-gray-400 rounded"></div>
+						<div class="h-4 bg-gray-400 rounded w-5/6"></div>
+						<div class="h-4 bg-gray-400 rounded w-3/4"></div>
+						<div class="h-4 bg-gray-400 rounded"></div>
+						<div class="h-4 bg-gray-400 rounded w-5/6"></div>
+						<div class="h-4 bg-gray-400 rounded w-3/4"></div>
+						<div class="h-4 bg-gray-400 rounded"></div>
+						<div class="h-4 bg-gray-400 rounded w-5/6"></div>
+						<div class="h-4 bg-gray-400 rounded w-3/4"></div>
+						<div class="h-4 bg-gray-400 rounded"></div>
+						<div class="h-4 bg-gray-400 rounded w-5/6"></div>
+						<div class="h-4 bg-gray-400 rounded w-3/4"></div>
+						<div class="h-4 bg-gray-400 rounded"></div>
+						<div class="h-4 bg-gray-400 rounded w-5/6"></div>
+					</div>
+				</div>
+			</div>
+		</article>
+	);
+}
